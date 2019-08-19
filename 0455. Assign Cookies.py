affa4f695533ee86d1ee9eb0aa8be6d8135c0a53
Weimin Zhou <wzhou007@ucr.edu>
2019-08-18 18:00:39 -0700

    Create 0455. Assign Cookies.py

---
+++ 0455. Assign Cookies.py
@@ -0,0 +1,38 @@
+# Solution 2, greedy, meet most greedy children first, O(n*log(n) + n)
+class Solution(object):
+    def findContentChildren(self, g, s):
+        g.sort()
+        s.sort()
+        
+        child = len(g)-1
+        for i in range(len(s)-1,-1,-1):
+            cookie = s[i]
+            while child >= 0 and g[child] > cookie:
+                child -= 1
+            if child < 0:
+                return len(s)-1-i
+            child -= 1 # large cookie is given to most greedy children
+        
+        return len(s)
+
+# Solution 1, greedy, meet least greedy children first, O(n*log(n) + n)
+class Solution1(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        g.sort()  # child
+        s.sort()  # cookie
+        
+        child = 0
+        
+        for i, cookie in enumerate(s):
+            if cookie >= g[child]:
+                child += 1
+            if child >= len(g):  # all children meeted
+                return child
+        
+        return child
+    
