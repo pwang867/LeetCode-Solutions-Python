c86ea8c868d97bcd642e7feefee58aa223229bbd
Weimin Zhou <wzhou007@ucr.edu>
2019-08-21 13:15:05 -0700

    Update 0874.Walking Robot Simulation.py

--- 0874.Walking Robot Simulation.py
+++ 0874.Walking Robot Simulation.py
@@ -7,7 +7,10 @@ class Solution(object):
         """
         cur = (0, 1)  # current direction
         pos = (0, 0)
-        for command in commands:
+        obstacles = set(map(tuple, obstacles))
+        max_dist = 0
+        for num in commands:
+            command = num
             if command < 0:
                 cur = self.getNextDirection(cur, command)
             else:
@@ -16,12 +19,15 @@ class Solution(object):
                         break
                     else:
                         pos = (pos[0]+cur[0], pos[1]+cur[1])
+                        max_dist = max(max_dist, pos[0]**2 + pos[1]**2)
                         command -= 1
-        return pos[0]**2 + pos[1]**2
+        
+        return max_dist  # return maximum distance, not final position distance
     
     def getNextDirection(self, cur, command):
         # use z1 = z*e^(i*theta) to get rotation matrix
-        # or we can use two dictionaries to get next direction d={(1,0):(0,1), ...}
+        # or we can use two dictionaries to get next direction 
+        # d={(1,0):(0,1), ...}
         if command == -2:  # rotate CCW
             return (-cur[1], cur[0])
         if command == -1:  # CW
