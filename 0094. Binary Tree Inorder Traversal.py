915fc4061b775ae33349bdb1b3604c95b0a88469
Weimin Zhou <wzhou007@ucr.edu>
2019-08-24 11:17:02 -0700

    Create 0094. Binary Tree Inorder Traversal.py

---
+++ 0094. Binary Tree Inorder Traversal.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# method 3: iteration
+class Solution(object):
+    def inorderTraversal(self, root):
+        res = []
+        
+        stack = []
+        node = root
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack.pop()
+                res.append(node.val)
+                node = node.right
+        
+        return res
+    
+
+# method 2: recursion with helper
+class Solution2(object):
+    def inorderTraversal(self, root):
+        self.res = []
+        self.inorderHelper(root)
+        return self.res
+    
+    def inorderHelper(self, root):
+        if not root:
+            return 
+        
+        self.inorderHelper(root.left)
+        self.res.append(root.val)
+        self.inorderHelper(root.right)
+        
+        
+
+
+# method 1: recursion without helper
+class Solution1(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        res = self.inorderTraversal(root.left)
+        res.append(root.val)
+        res.extend(self.inorderTraversal(root.right))
+        return res
+    
