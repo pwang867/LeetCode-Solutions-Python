5698beeac33d8d028df2413edaebb68f5393a460
Weimin Zhou <wzhou007@ucr.edu>
2019-08-24 10:58:32 -0700

    Update 0783. Minimum Distance Between BST Nodes.py

--- 0783. Minimum Distance Between BST Nodes.py
+++ 0783. Minimum Distance Between BST Nodes.py
@@ -5,7 +5,33 @@
 #         self.left = None
 #         self.right = None
 
+# iteration, in order traversal
 class Solution(object):
+    def minDiffInBST(self, root):
+        if not root:
+            return 0
+        
+        stack = []  # store the path
+        node = root  # to be processed
+        res = float('inf')
+        pre = -float('inf')
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                # every node before the popped node is already processed
+                # in order traversal
+                node = stack.pop()  
+                res = min(res, node.val - pre)
+                pre = node.val  # do not forget!!
+                node = node.right
+        return res
+    
+
+# recursion, O(n)
+# return the min and max of a root
+class Solution1(object):
     def minDiffInBST(self, root):
         """
         :type root: TreeNode
