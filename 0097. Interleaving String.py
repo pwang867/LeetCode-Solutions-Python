226fa03f407ec40b644bcbb4092fe44ff20e4ca7
Weimin Zhou <wzhou007@ucr.edu>
2019-08-26 20:26:06 -0700

    Update 0097. Interleaving String.py

--- 0097. Interleaving String.py
+++ 0097. Interleaving String.py
@@ -1,9 +1,32 @@
-
-
-
-
-# recursion with memo, O(len(s1)*len(s2))
+# method 2: dp
 class Solution(object):
+    def isInterleave(self, s1, s2, s3):
+        m, n = len(s1), len(s2)
+        if m + n != len(s3):  # do not forget, otherwise s3[i+j-1] will be invalid
+            return False
+        
+        # dp[i][j] means whether s1[:i] and s2[:j] matches with s3[:i+j]
+        dp = [[False]*(n+1) for _ in range(m+1)]
+        dp[0][0] = True
+        for j in range(1, n+1):
+            dp[0][j] = (s2[j-1]==s3[j-1])
+            if dp[0][j] is False:
+                break
+        for i in range(1, m+1):
+            dp[i][0] = (s1[i-1]==s3[i-1])
+            if dp[i][0] is False:
+                break
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                dp[i][j] = (dp[i-1][j] and s1[i-1]==s3[i+j-1]) \
+                            or (dp[i][j-1] and s2[j-1]==s3[i+j-1])
+        
+        return dp[m][n]
+        
+
+
+# recursion with memo
+class Solution1(object):
     def isInterleave(self, s1, s2, s3):
         """
         :type s1: str
@@ -11,12 +34,7 @@ class Solution(object):
         :type s3: str
         :rtype: bool
         """
-        # memo is to save failure matches
-        # when s1[:i], s2[:j] matches with s3[:i+j], 
-        # but s1[i:], s2[j:] don't match with s3[i+j:], then save (i, j)
-        # so that we don't check this pair again
-        self.memo = set()  
-        
+        self.memo = set()
         if len(s1) + len(s2) != len(s3):
             return False
         
