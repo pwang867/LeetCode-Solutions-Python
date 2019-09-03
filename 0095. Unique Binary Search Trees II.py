bd535c1991fec03881694ff3cef4ceee1e4d9031
Weimin Zhou <wzhou007@ucr.edu>
2019-09-02 23:44:33 -0700

    Update 0095. Unique Binary Search Trees II.py

--- 0095. Unique Binary Search Trees II.py
+++ 0095. Unique Binary Search Trees II.py
@@ -5,6 +5,28 @@
 #         self.left = None
 #         self.right = None
 
+# method 2: dp, add integers one by one
+class Solution(object):
+    def generateTrees(self, n):
+        if n == 0:
+            return [None]
+        dp = [TreeNode(1)]
+        for i in range(2, n+1):
+            new_dp = []
+            for node in dp:
+                root1 = TreeNode(i)
+                root1.left = node
+                new_dp.append(root1)
+                root2 = node
+                while node.right:
+                    node = node.right
+                node.right = TreeNode(i)
+                new_dp.append(root2)
+            dp = new_dp
+        return dp
+
+
+# method 1: recursion
 class Solution(object):
     def generateTrees(self, n):
         """
@@ -15,19 +37,19 @@ class Solution(object):
             return []
         
         arr = [i for i in range(1, n+1)]
-        return self.BSTHelper(arr, 0, n-1)
+        return self.BSTHelper(1, n)
     
-    def BSTHelper(self, arr, low, high):
+    def BSTHelper(self, low, high):
         if low > high:
             return [None]
         
         trees = []
         for i in range(low, high+1):
-            left_trees = self.BSTHelper(arr, low, i-1)
-            right_trees = self.BSTHelper(arr, i+1, high)
+            left_trees = self.BSTHelper(low, i-1)
+            right_trees = self.BSTHelper(i+1, high)
             for ltree in left_trees:
                 for rtree in right_trees:
-                    root = TreeNode(arr[i])
+                    root = TreeNode(i)
                     root.left = ltree
                     root.right = rtree
                     trees.append(root)
