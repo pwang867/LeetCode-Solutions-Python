541ab3eae58c40eb914a71104b2f48d1511d15b4
Weimin Zhou <wzhou007@ucr.edu>
2019-09-06 23:17:34 -0700

    Create 0472. Concatenated Words.py

---
+++ 0472. Concatenated Words.py
@@ -0,0 +1,40 @@
+# time complexity: O(N*M^M), where N is len(words), M is len(words[i])
+# space O(N+M) for a wordset, and a self.visited to store index 
+# time limit exceeded
+class Solution(object):
+    def findAllConcatenatedWordsInADict(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        wordset = set(words)
+        res = []
+        for word in words:
+            if not word:  # when word is ""
+                continue
+            wordset.remove(word)
+            self.visited = set()
+            
+            if self.checkConcat(word, 0, wordset):
+                res.append(word)
+            
+            wordset.add(word)
+            
+        return res
+    
+    def checkConcat(self, word, i, words):
+        # basic DFS
+        if i == len(word):
+            return True
+        
+        if i in self.visited:
+            return False
+        for j in range(len(word)-1, i-1, -1):
+            # to search from longer substring to shorter to reduce recursion depth
+            if word[i:j+1] in words and self.checkConcat(word, j+1, words):
+                return True
+        
+        self.visited.add(i)
+        return False
+    
+                
