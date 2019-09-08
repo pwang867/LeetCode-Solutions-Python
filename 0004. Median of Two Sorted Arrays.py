9239f41db717e8239d04306791aaf829e86bbb7e
Weimin Zhou <wzhou007@ucr.edu>
2019-09-07 19:28:54 -0700

    Update 0004. Median of Two Sorted Arrays.py

--- 0004. Median of Two Sorted Arrays.py
+++ 0004. Median of Two Sorted Arrays.py
@@ -1,5 +1,4 @@
 # method 3, do a binary search for i only, j is defined by (m+n)//2 - i
-# https://leetcode.com/problems/median-of-two-sorted-arrays/
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
         """
@@ -10,8 +9,8 @@ class Solution(object):
         if len(nums1) > len(nums2):  # to guarantee j = (m + n)//2 - i is valid
             nums1, nums2 = nums2, nums1
         
-        m, n = len(nums1), len(nums2)
-        imin, imax = 0, m
+        m, n = len(nums1), len(nums2)  # m <= n
+        imin, imax = 0, m  # not m-1 because we will consider nums1[i] and nums1[i-1]
         N = (m + n)//2
         
         while imin <= imax:
@@ -28,7 +27,7 @@ class Solution(object):
                 else:
                     return (min(A2, B2) + max(A1, B1))/2.0
             elif A1 > B2:
-                imax = i - 1
+                imax = i - 1  # when i == 0, A1=-float('inf'), so we will never have A1 > B2
             elif B1 > A2:
                 imin = i + 1
         
@@ -120,3 +119,19 @@ class Solution(object):
 #         else:
 #             return self.findKthValue(nums1, nums2, i, j + block, k - block)
         
+
+
+"""
+# https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+You may assume nums1 and nums2 cannot be both empty.
+
+Example 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+"""
