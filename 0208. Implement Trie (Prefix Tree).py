6c4f92c1680bd4bee224d6814e39f00fd2ea2549
Weimin Zhou <wzhou007@ucr.edu>
2019-09-12 22:31:15 -0700

    Update 0208. Implement Trie (Prefix Tree).py

--- 0208. Implement Trie (Prefix Tree).py
+++ 0208. Implement Trie (Prefix Tree).py
@@ -1,5 +1,6 @@
-# method 1: simply use a dictionary of dictionaries
+# method 2: simply use a dictionary of dictionaries
 # to construct the Trie
+# simpler and faster than method 1
 
 class Trie1(object):
 
@@ -32,3 +33,46 @@ class Trie1(object):
                 return False
             cur = cur[c]
         return True
+
+
+
+# method 1: use an extra class TrieNode 
+# to help implement the Trie
+class TrieNode(object):
+    
+    def __init__(self):
+        self.is_word = False
+        self.children = {}  # {letter: TrieNode}
+
+class Trie(object):
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.is_word = True
+        
+
+    def search(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return node.is_word
+        
+
+    def startsWith(self, prefix):
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+        return True
+    
