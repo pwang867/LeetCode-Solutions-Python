0bbb68cb2d2805de622134e17165dbc466ec29ec
Weimin Zhou <wzhou007@ucr.edu>
2019-09-22 17:56:39 -0700

    Update 0482. License Key Formatting.py

--- 0482. License Key Formatting.py
+++ 0482. License Key Formatting.py
@@ -1,25 +1,28 @@
 # method 2: use a single while loop
+# loop from behind because the first block might be be length K
+# remove extra "-" in the front if any
+from collections import deque
 class Solution(object):
     def licenseKeyFormatting(self, S, K):
-        res = []
+        res = deque([])
         
         i = len(S) - 1
         cnt = 0
         while i >= 0:
             if cnt == K:
-                res.append("-")
+                res.appendleft("-")
                 cnt = 0
             if S[i] != "-":
-                res.append(S[i].upper())
+                res.appendleft(S[i].upper())
                 cnt += 1
             i -= 1
         
         # easy to make mistake, fail on S = "5F3Z-2e-9-w", K = 4
-        if res and res[-1] == "-":  
-            res.pop()
+        if res and res[0] == "-":  
+            res.popleft()
         
-        res.reverse()
         return "".join(res)
+            
             
 
 # method 1: two slow due to too many function calls of self.getLastNChar
