8a9fdb7f17dbb612e5e5703ca28a038eb9c8d723
Weimin Zhou <wzhou007@ucr.edu>
2019-09-22 17:15:03 -0700

    Update 1007. Minimum Domino Rotations For Equal Row.py

--- 1007. Minimum Domino Rotations For Equal Row.py
+++ 1007. Minimum Domino Rotations For Equal Row.py
@@ -1,3 +1,4 @@
+# time O(n), space O(1)
 # we need to switch A[i] and B[i] until A or B is filled with only one common number
 # the common number must be either A[0] or B[0], or other pairs (A[1], B[1])
 # for each candidate, we need to consider 
@@ -12,21 +13,14 @@ class Solution(object):
         """
         if not A:
             return 0
-        if len(A) != len(B):
-            return -1
         
-        res = float('inf')
-        
-        # if the common number is A[0]
+        # check if the common number could be A[0]
         num_rotation = self.getNumOfRotation(A, B, A[0])
         if num_rotation != -1:  # -1 means rotation is not possible
-            res = min(res, num_rotation)
+            return num_rotation  # don't have to check B[0] any more
         
-        num_rotation = self.getNumOfRotation(B, A, B[0])
-        if num_rotation != -1:
-            res = min(res, num_rotation)
-        
-        return res if res != float('inf') else -1
+        # check if the common number could be B[0]
+        return self.getNumOfRotation(B, A, B[0])
     
     def getNumOfRotation(self, A, B, candidate):
         cntA, cntB = 0, 0
@@ -37,7 +31,6 @@ class Solution(object):
                 cntB += 1
             if A[i] != candidate and B[i] != candidate:
                 return -1
-        
         # the candidate may be aligned in the upper or lower row
         return min(len(A)-cntA, len(A)-cntB)  
     
